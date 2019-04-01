35bf73be294c2f765ebaf9606601fdd9c04587d7
lab <labcomp@labs-imac-2.dhcp.nd.edu>
2019-04-01 17:29:07 -0400

    update ExpB

--- src/baseline_A.m
+++ src/baseline_A.m
@@ -18,7 +18,7 @@
 
 % clear; close all; clc;
 Exp_ID = 'ExpA2'
-Env_ID = 'Env1'
+Env_ID = 'Env4'
 
 % add required libraries to the path
 addpath(genpath('utility'));
@@ -29,9 +29,9 @@ vlfeat_path = fullfile('.','vlfeat-0.9.21','toolbox','vl_setup');
 run(vlfeat_path);
 
 % set paths to the wave files and protocols
-pathToTrainData = fullfile('..','data','ASVspoof2017_train');
+pathToTrainData = fullfile('..','data','ASVspoof2017_train_dev');
 pathToEvalData = fullfile('..','data',Env_ID);
-trainProtocolFile = fullfile('..','data','ASVspoof2017_protocol', 'ASVspoof2017_train.txt');
+trainProtocolFile = fullfile('..','data','ASVspoof2017_protocol', 'ASVspoof2017_train_dev.txt');
 evalProtocolFile = fullfile('..','metadata',strcat(Env_ID,'_meta'),strcat(Env_ID,'_meta_aligned.csv'));
 
 % set save path:
@@ -55,53 +55,53 @@ spoofIdx = find(strcmp(labels,'spoof'));
 
 %% Feature extraction for training data
 
-% % extract features for GENUINE training data and store in cell array
-% disp('Extracting features for GENUINE training data...');
-% genuineFeatureCell = cell(size(genuineIdx));
-% parfor i=1:length(genuineIdx)
-%     filePath = fullfile(pathToTrainData,filelist{genuineIdx(i)});
-%     [x,fs] = audioread(filePath);
-% 
-%     tmp_fea = cqcc(x, fs, 96, fs/2, fs/2^10, 16, 29, 'ZsdD');
-%     genuineFeatureCell{i} = tmp_fea;
-%     
-%     save_name = strrep(filelist{genuineIdx(i)}, '.wav', '_cqcc.mat');
-%     save_path = fullfile(TrainFeatureSavePath, save_name);
-%     parsave(save_path, tmp_fea);
-% end
-% disp('Done!');
-% 
-% % extract features for SPOOF training data and store in cell array
-% disp('Extracting features for SPOOF training data...');
-% spoofFeatureCell = cell(size(spoofIdx));
-% parfor i=1:length(spoofIdx)
-%     filePath = fullfile(pathToTrainData,filelist{spoofIdx(i)});
-%     [x,fs] = audioread(filePath);
-% 
-%     tmp_fea = cqcc(x, fs, 96, fs/2, fs/2^10, 16, 29, 'ZsdD');
-%     spoofFeatureCell{i} = tmp_fea;
-%     
-%     save_name = strrep(filelist{spoofIdx(i)}, '.wav', '_cqcc.mat');
-%     save_path = fullfile(TrainFeatureSavePath, save_name);
-%     parsave(save_path, tmp_fea);
-% end
-% disp('Done!');
+% extract features for GENUINE training data and store in cell array
+disp('Extracting features for GENUINE training data...');
+genuineFeatureCell = cell(size(genuineIdx));
+parfor i=1:length(genuineIdx)
+    filePath = fullfile(pathToTrainData,filelist{genuineIdx(i)});
+    [x,fs] = audioread(filePath);
+
+    tmp_fea = cqcc(x, fs, 96, fs/2, fs/2^10, 16, 29, 'ZsdD');
+    genuineFeatureCell{i} = tmp_fea;
+    
+    save_name = strrep(filelist{genuineIdx(i)}, '.wav', '_cqcc.mat');
+    save_path = fullfile(TrainFeatureSavePath, save_name);
+    parsave(save_path, tmp_fea);
+end
+disp('Done!');
+
+% extract features for SPOOF training data and store in cell array
+disp('Extracting features for SPOOF training data...');
+spoofFeatureCell = cell(size(spoofIdx));
+parfor i=1:length(spoofIdx)
+    filePath = fullfile(pathToTrainData,filelist{spoofIdx(i)});
+    [x,fs] = audioread(filePath);
+
+    tmp_fea = cqcc(x, fs, 96, fs/2, fs/2^10, 16, 29, 'ZsdD');
+    spoofFeatureCell{i} = tmp_fea;
+    
+    save_name = strrep(filelist{spoofIdx(i)}, '.wav', '_cqcc.mat');
+    save_path = fullfile(TrainFeatureSavePath, save_name);
+    parsave(save_path, tmp_fea);
+end
+disp('Done!');
 
 %% GMM training
 
-% % train GMM for GENUINE data
-% disp('Training GMM for GENUINE...');
-% [genuineGMM.m, genuineGMM.s, genuineGMM.w] = vl_gmm([genuineFeatureCell{:}], 512, 'verbose', 'MaxNumIterations',100);
-% gen_save_path = fullfile(GmmSavePath,'genuineGMM.mat');
-% save(gen_save_path,'-struct','genuineGMM');
-% disp('Done!');
-% 
-% % train GMM for SPOOF data
-% disp('Training GMM for SPOOF...');
-% [spoofGMM.m, spoofGMM.s, spoofGMM.w] = vl_gmm([spoofFeatureCell{:}], 512, 'verbose', 'MaxNumIterations',100);
-% spf_save_path = fullfile(GmmSavePath,'spoofGMM.mat');
-% save(spf_save_path,'-struct','spoofGMM');
-% disp('Done!');
+% train GMM for GENUINE data
+disp('Training GMM for GENUINE...');
+[genuineGMM.m, genuineGMM.s, genuineGMM.w] = vl_gmm([genuineFeatureCell{:}], 512, 'verbose', 'MaxNumIterations',100);
+gen_save_path = fullfile(GmmSavePath,'genuineGMM.mat');
+save(gen_save_path,'-struct','genuineGMM');
+disp('Done!');
+
+% train GMM for SPOOF data
+disp('Training GMM for SPOOF...');
+[spoofGMM.m, spoofGMM.s, spoofGMM.w] = vl_gmm([spoofFeatureCell{:}], 512, 'verbose', 'MaxNumIterations',100);
+spf_save_path = fullfile(GmmSavePath,'spoofGMM.mat');
+save(spf_save_path,'-struct','spoofGMM');
+disp('Done!');
 
 
 %% Feature extraction and scoring of ReMASC data
@@ -121,8 +121,8 @@ disp('Computing scores for evaluation trials...');
 h = waitbar(0,'please wait');
 l = length(filelist);
 for i=1:length(filelist)
-%     tmp_fname = strcat(int2str(filelist(i)), '.wav');
-    tmp_fname = strcat(sprintf('%06d',filelist(i)),'.wav'); %for env1 only!!!
+    tmp_fname = strcat(int2str(filelist(i)), '.wav');
+%     tmp_fname = strcat(sprintf('%06d',filelist(i)),'.wav'); %for env1 only!!!
     filePath = fullfile(pathToEvalData, tmp_fname);
 %     [x,fs] = audioread(filePath);
     
